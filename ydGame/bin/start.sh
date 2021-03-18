a454fcc4f1655ca89d320da658c911b4313d664b
dtgg <562178925@qq.com>
2021-03-18 18:55:15 +0800

    修改启动脚本

--- ydGame/bin/start.sh
+++ ydGame/bin/start.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-nohup java -jar -Xmx2096m -Xms2096m -Xmn1536m -Xss1m -XX:PermSize=1024m -XX:MaxPermSize=1024m -XX:+UseConcMarkSweepGC -XX:CMSInitiatingOccupancyFraction=85 -XX:+UseCMSCompactAtFullCollection -XX:CMSFullGCsBeforeCompaction=0 -XX:+CMSParallelRemarkEnabled -XX:+CMSClassUnloadingEnabled /usr/local/games/bs4rummy-1.0.jar >/usr/local/games/bs4rummy.log 2>&1 &
+nohup java -jar -Xmx8096m -Xms8096m -Xmn1536m -Xss1m -XX:PermSize=1024m -XX:MaxPermSize=1024m -XX:+UseConcMarkSweepGC -XX:CMSInitiatingOccupancyFraction=85 -XX:+UseCMSCompactAtFullCollection -XX:CMSFullGCsBeforeCompaction=0 -XX:+CMSParallelRemarkEnabled -XX:+CMSClassUnloadingEnabled /usr/local/games/bs4lottery-1.0.jar >/usr/local/games/bs4lottery.log 2>&1 &
 
-nohup java -jar -Xmx5096m -Xms5096m -Xmn1536m -Xss1m -XX:PermSize=1024m -XX:MaxPermSize=1024m -XX:+UseConcMarkSweepGC -XX:CMSInitiatingOccupancyFraction=85 -XX:+UseCMSCompactAtFullCollection -XX:CMSFullGCsBeforeCompaction=0 -XX:+CMSParallelRemarkEnabled -XX:+CMSClassUnloadingEnabled /usr/local/games/bs4lottery-1.0.jar >/usr/local/games/bs4lottery.log 2>&1 &
-
-nohup java -jar -Xmx4096m -Xms4096m -Xmn1536m -Xss1m -XX:PermSize=1024m -XX:MaxPermSize=1024m -XX:+UseConcMarkSweepGC -XX:CMSInitiatingOccupancyFraction=85 -XX:+UseCMSCompactAtFullCollection -XX:CMSFullGCsBeforeCompaction=0 -XX:+CMSParallelRemarkEnabled -XX:+CMSClassUnloadingEnabled /usr/local/games/lobby-1.0.jar >/usr/local/games/lobby.log 2>&1 &
+nohup java -jar -Xmx5096m -Xms5096m -Xmn1536m -Xss1m -XX:PermSize=1024m -XX:MaxPermSize=1024m -XX:+UseConcMarkSweepGC -XX:CMSInitiatingOccupancyFraction=85 -XX:+UseCMSCompactAtFullCollection -XX:CMSFullGCsBeforeCompaction=0 -XX:+CMSParallelRemarkEnabled -XX:+CMSClassUnloadingEnabled /usr/local/games/lobby-1.0.jar >/usr/local/games/lobby.log 2>&1 &
 
 nohup java -jar -Xmx4096m -Xms4096m -Xmn1536m -Xss1m -XX:PermSize=1024m -XX:MaxPermSize=1024m -XX:+UseConcMarkSweepGC -XX:CMSInitiatingOccupancyFraction=85 -XX:+UseCMSCompactAtFullCollection -XX:CMSFullGCsBeforeCompaction=0 -XX:+CMSParallelRemarkEnabled -XX:+CMSClassUnloadingEnabled /usr/local/games/connection-1.0.jar >/usr/local/games/connection.log 2>&1 &
 
