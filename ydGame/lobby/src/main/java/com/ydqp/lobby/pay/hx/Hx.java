4172777f8bb162bee3251911bb26741b7eacbd5c
dtgg <562178925@qq.com>
2021-03-26 19:24:49 +0800

    hx支付

--- ydGame/lobby/src/main/java/com/ydqp/lobby/pay/hx/Hx.java
+++ ydGame/lobby/src/main/java/com/ydqp/lobby/pay/hx/Hx.java
@@ -13,6 +13,7 @@ import com.ydqp.lobby.pay.PayUrlUtil;
 import com.ydqp.lobby.service.PlayerService;
 import com.ydqp.lobby.utils.Md5Utils;
 import com.ydqp.lobby.utils.RandomValueUtil;
+import jodd.util.URLDecoder;
 import org.apache.commons.codec.digest.DigestUtils;
 
 import java.math.BigDecimal;
@@ -36,14 +37,14 @@ public class Hx extends OrderPay {
         params.put("order_sn", order.getOrderId());
 
         BigDecimal amount = BigDecimal.valueOf(order.getAmount()).setScale(2, BigDecimal.ROUND_DOWN);
-        params.put("amount", amount);
+        params.put("amount", amount.toString());
         params.put("name", RandomValueUtil.getName());
-        params.put("email", RandomValueUtil.getEmail(8,10));
 
         Player player = PlayerService.getInstance().queryByPlayerId(order.getPlayerId());
+        params.put("email", player.getPlayerName() + "@gmail.com");
         params.put("phone", player.getPlayerName());
         params.put("remark", "Recharge");
-        params.put("redirect_url", "https://nrly.paysempbf.com/api/redirect");
+        params.put("redirect_url", "http://whw.ngrok2.xiaomiqiu.cn/api/redirect");
 
         String signStr = config.getAppId() + order.getOrderId() + amount + config.getSecretKey();
         logger.info("payment sign str:{}", signStr);
@@ -60,21 +61,23 @@ public class Hx extends OrderPay {
         JSONObject response = JSONObject.parseObject(result);
 
         PlayerOrderSuccess playerOrderSuccess = new PlayerOrderSuccess();
-        if (response.getInteger("success") == 0) {
-            order.setStatus(2);
-            order.setMsg(response.getString("message"));
+        if (response.getInteger("code") == 0) {
+            JSONObject responseData = response.getJSONObject("data");
+            order.setTxnOrderId(responseData.getString("platform_osn"));
 
-            playerOrderSuccess.setSuccess(false);
-            playerOrderSuccess.setMessage("Recharge failed!");
+            JSONObject data = new JSONObject();
+            data.put("url", URLDecoder.decode(responseData.getString("pay_url")));
+            playerOrderSuccess.setData(data.toJSONString());
+            playerOrderSuccess.setSuccess(true);
+            playerOrderSuccess.setPayChannel("url");
+            return playerOrderSuccess;
         }
 
-        order.setTxnOrderId(response.getString("ticket"));
+        order.setStatus(2);
+        order.setMsg(response.getString("message"));
 
-        playerOrderSuccess.setSuccess(true);
-        playerOrderSuccess.setPayChannel("url");
-        JSONObject data = new JSONObject();
-        data.put("url", response.getString("pageurl"));
-        playerOrderSuccess.setData(data.toJSONString());
+        playerOrderSuccess.setSuccess(false);
+        playerOrderSuccess.setMessage("Recharge failed!");
         return playerOrderSuccess;
     }
 
@@ -89,7 +92,7 @@ public class Hx extends OrderPay {
         params.put("order_sn", withdrawal.getTransferId());
         BigDecimal am = new BigDecimal(String.valueOf(withdrawal.getAmount()));
         BigDecimal amount = am.multiply(BigDecimal.ONE.subtract(config.getWithdrawFee())).setScale(2, RoundingMode.DOWN);
-        params.put("amount", amount);
+        params.put("amount", amount.toString());
         params.put("type", "bank");
         params.put("name", account.getName());
         params.put("account", account.getAccNo());
@@ -111,18 +114,20 @@ public class Hx extends OrderPay {
         JSONObject response = JSONObject.parseObject(result);
 
         PlayerWithdrawalSuccess playerWithdrawalSuccess = new PlayerWithdrawalSuccess();
-        if (response.getInteger("success") == 0) {
-            withdrawal.setStatus(2);
-            withdrawal.setErrorMsg(response.getString("message"));
+        if (response.getInteger("code") == 0) {
+            JSONObject responseData = response.getJSONObject("data");
+            withdrawal.setReferenceId(responseData.getString("platform_osn"));
+            withdrawal.setStatus(1);
 
-            playerWithdrawalSuccess.setSuccess(false);
-            playerWithdrawalSuccess.setMessage("Withdrawal failed!");
+            playerWithdrawalSuccess.setSuccess(true);
+            return playerWithdrawalSuccess;
         }
 
-        withdrawal.setReferenceId(response.getString("ticket"));
-        withdrawal.setStatus(1);
+        withdrawal.setStatus(2);
+        withdrawal.setErrorMsg(response.getString("message"));
 
-        playerWithdrawalSuccess.setSuccess(true);
+        playerWithdrawalSuccess.setSuccess(false);
+        playerWithdrawalSuccess.setMessage("Withdrawal failed!");
         return playerWithdrawalSuccess;
     }
 
@@ -130,10 +135,12 @@ public class Hx extends OrderPay {
 //        System.out.println(DigestUtils.md5Hex("512b2e1d-0173-415a-95bd-e3585fc75fb6"+"20200101123456-A001" + "3000"));
 
         PayChannelConfig config = new PayChannelConfig();
+//        PayWithdrawalConfig config = new PayWithdrawalConfig();
         config.setName("hx");
         config.setMchId("20210320-odxlk");
         config.setAppId("0a7e1867c26e0a2fedd46ea02d0571dc");
         config.setSecretKey("2a6284676b8ee97beaa5de20e4501605");
+
         config.setClientId("0a7e1867c26e0a2fedd46ea02d0571dc");
         config.setClientSecret("2a6284676b8ee97beaa5de20e4501605");
         config.setWithdrawFee(new BigDecimal("0.02"));
@@ -145,14 +152,14 @@ public class Hx extends OrderPay {
 
         new CommonPay().getOrderPay(config.getName()).payment(order, config);
 
-//        PlayerWithdrawal withdrawal = new PlayerWithdrawal();
-//        withdrawal.setTransferId(UUID.randomUUID().toString().replace("-", ""));
-//        withdrawal.setAmount(100);
-//
-//        PlayerAccount account = new PlayerAccount();
-//        account.setName("zhangsan");
-//        account.setAccNo("123456");
-//        account.setIfsc("111");
+        PlayerWithdrawal withdrawal = new PlayerWithdrawal();
+        withdrawal.setTransferId(UUID.randomUUID().toString().replace("-", ""));
+        withdrawal.setAmount(10);
+
+        PlayerAccount account = new PlayerAccount();
+        account.setName("Anandhu Sabu");
+        account.setAccNo("67304084219");
+        account.setIfsc("SBIN0000000");
 //        new CommonPay().getOrderPay(config.getName()).payout(withdrawal, config, account);
     }
 }
