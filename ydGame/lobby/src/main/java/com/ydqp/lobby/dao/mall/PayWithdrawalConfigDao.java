b45309213bc7832b8da2fd1f6846ea76b8dbae0f
dtgg <562178925@qq.com>
2021-04-01 14:43:01 +0800

    修改短信模板

--- ydGame/lobby/src/main/java/com/ydqp/lobby/dao/mall/PayWithdrawalConfigDao.java
+++ ydGame/lobby/src/main/java/com/ydqp/lobby/dao/mall/PayWithdrawalConfigDao.java
@@ -16,30 +16,30 @@ public class PayWithdrawalConfigDao {
         return instance;
     }
 
-    private static final String PAY_CONFIG = "select * from pay_channel_config where enabled = 1 limit 1;";
+    private static final String PAY_CONFIG = "select * from pay_withdrawal_config where enabled = 1 limit 1;";
     public PayWithdrawalConfig getPayChannelConfig() {
         return (PayWithdrawalConfig) JdbcOrm.getInstance().getBean(PAY_CONFIG, PayWithdrawalConfig.class);
     }
 
-    private static final String FIND_BY_CHANNEL = "select * from pay_channel_config where enabled = 1 and channel = ";
+    private static final String FIND_BY_CHANNEL = "select * from pay_withdrawal_config where enabled = 1 and channel = ";
     public PayWithdrawalConfig findByChannel(int channel) {
         String sql = FIND_BY_CHANNEL + channel + ";";
         return (PayWithdrawalConfig) JdbcOrm.getInstance().getBean(sql, PayWithdrawalConfig.class);
     }
 
-    private static final String FIND_BY_NAME = "select * from pay_channel_config where name = '";
+    private static final String FIND_BY_NAME = "select * from pay_withdrawal_config where name = '";
     public PayWithdrawalConfig findByName(String name) {
         String sql = FIND_BY_NAME + name + "';";
         return (PayWithdrawalConfig) JdbcOrm.getInstance().getBean(sql, PayWithdrawalConfig.class);
     }
 
-    private static final String ENABLE_PAY_CONFIG = "select * from pay_channel_config where enabled = 1;";
+    private static final String ENABLE_PAY_CONFIG = "select * from pay_withdrawal_config where enabled = 1;";
     public List<PayWithdrawalConfig> getAllEnableChannel() {
         return JdbcOrm.getInstance().getListBean(ENABLE_PAY_CONFIG, PayWithdrawalConfig.class);
     }
 
     public PayWithdrawalConfig findById(int id) {
-        String sql = "select * from pay_channel_config where id = " + id + ";";
+        String sql = "select * from pay_withdrawal_config where id = " + id + ";";
         return (PayWithdrawalConfig) JdbcOrm.getInstance().getBean(sql, PayWithdrawalConfig.class);
     }
 }
