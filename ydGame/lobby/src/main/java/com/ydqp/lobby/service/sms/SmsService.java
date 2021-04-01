32dcca190a3660ad9e21b40dea3a38b756c3a84e
dtgg <562178925@qq.com>
2021-04-01 13:58:27 +0800

    修改短信模板

--- ydGame/lobby/src/main/java/com/ydqp/lobby/service/sms/SmsService.java
+++ ydGame/lobby/src/main/java/com/ydqp/lobby/service/sms/SmsService.java
@@ -112,7 +112,7 @@ public class SmsService {
         param.setNumbers(areaCode + phone);
         param.setSenderid("");
         int smsCode = (int) ((Math.random() * 9 + 1) * 1000);
-        param.setContent("[coco] SMS verification code is {"+smsCode+"}, valid for 5 minutes, please don't tell others.");
+        param.setContent("[coco] SMS verification code is "+smsCode+", valid for 5 minutes, please don't tell others.");
         logger.info("请求地址为: {};请求参数为: {}", requestUrl, JSON.toJSONString(param));
         try {
             String result = HttpSmsUtil.post(requestUrl, JSON.toJSONString(param));
