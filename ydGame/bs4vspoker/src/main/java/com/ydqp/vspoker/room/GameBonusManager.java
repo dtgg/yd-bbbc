6f70033648e7ff354a8a7cd374d60be62f6a790f
dtgg <562178925@qq.com>
2021-05-08 11:50:14 +0800

    修改奖励配置

--- ydGame/bs4vspoker/src/main/java/com/ydqp/vspoker/room/GameBonusManager.java
+++ ydGame/bs4vspoker/src/main/java/com/ydqp/vspoker/room/GameBonusManager.java
@@ -27,16 +27,16 @@ public class GameBonusManager {
         gameBonusMap.put(1, freeGameBonusMap);
 
         Map<Integer, Double> signInGameBonusMap = new HashMap<>();
-        freeGameBonusMap.put(1, 2000D);
-        freeGameBonusMap.put(2, 1200D);
-        freeGameBonusMap.put(3, 800D);
-        freeGameBonusMap.put(4, 600D);
-        freeGameBonusMap.put(5, 400D);
-        freeGameBonusMap.put(6, 200D);
-        freeGameBonusMap.put(7, 200D);
-        freeGameBonusMap.put(8, 200D);
-        freeGameBonusMap.put(9, 200D);
-        freeGameBonusMap.put(10, 200D);
+        signInGameBonusMap.put(1, 2000D);
+        signInGameBonusMap.put(2, 1200D);
+        signInGameBonusMap.put(3, 800D);
+        signInGameBonusMap.put(4, 600D);
+        signInGameBonusMap.put(5, 400D);
+        signInGameBonusMap.put(6, 200D);
+        signInGameBonusMap.put(7, 200D);
+        signInGameBonusMap.put(8, 200D);
+        signInGameBonusMap.put(9, 200D);
+        signInGameBonusMap.put(10, 200D);
         gameBonusMap.put(2, signInGameBonusMap);
     }
 
